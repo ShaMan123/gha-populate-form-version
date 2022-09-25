5cbce6cc41d7602708d727f98cadf8d993ab0ea2
ShaMan123 <shacharnen@gmail.com>
2022-09-26 02:54:21 +0300

    github registry

--- src/index.js
+++ src/index.js
@@ -1,42 +1,34 @@
 import { getInput, setFailed, setOutput } from '@actions/core';
-import cp from 'node:child_process';
 import { listTags, writeYAML } from './util';
 
-try {
-	const form = getInput('form', { trimWhitespace: true, required: true });
-	const packageName = getInput('package', {
-		trimWhitespace: true,
-		required: true,
-	});
-	const registry = getInput('registry', {
-		trimWhitespace: true,
-		required: true,
-	});
-	const order = getInput('order', { trimWhitespace: true, required: true });
-	const limitTo =
-		Math.abs(Number(getInput('limit_to', { trimWhitespace: true }))) ||
-		undefined;
-	const dropdownId = getInput('dropdown', {
-		trimWhitespace: true,
-		required: true,
-	});
-	const commitMessage = getInput('commit_message', {
-		trimWhitespace: true,
-		required: true,
-	});
-	const tags =
-		getInput('tags', { trimWhitespace: true }) ||
-		listTags(registry, packageName, order, limitTo);
-	setOutput('tags', tags);
-	writeYAML(form, dropdownId, tags);
-	// cp.execSync(`git config --global user.name github-actions[bot]`);
-	// cp.execSync(
-	// 	`git config --global user.email github-actions[bot]@users.noreply.github.com`,
-	// );
-	// cp.execSync(`git add ${form}`);
-	// cp.execSync(`git commit -m "${commitMessage}"`);
-	// cp.execSync(`git push`);
-} catch (error) {
-	console.log(error);
-	setFailed(error);
+async function run() {
+	try {
+		const form = getInput('form', { trimWhitespace: true, required: true });
+		const packageName = getInput('package', {
+			trimWhitespace: true,
+			required: true,
+		});
+		const registry = getInput('registry', {
+			trimWhitespace: true,
+			required: true,
+		});
+		const order = getInput('order', { trimWhitespace: true, required: true });
+		const limitTo =
+			Math.abs(Number(getInput('limit_to', { trimWhitespace: true }))) ||
+			undefined;
+		const dropdownId = getInput('dropdown', {
+			trimWhitespace: true,
+			required: true,
+		});
+		const tags =
+			getInput('tags', { trimWhitespace: true }) ||
+			(await listTags(registry, packageName, order, limitTo));
+		setOutput('tags', tags);
+		writeYAML(form, dropdownId, tags);
+	} catch (error) {
+		console.log(error);
+		setFailed(error);
+	}
 }
+
+run();
--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import { getInput } from '@actions/core';
+import github from '@actions/github';
 import fs from 'fs';
 import YAML from 'js-yaml';
 import cp from 'node:child_process';
@@ -7,13 +9,25 @@ function listNPMTags(packageName) {
 		cp.execSync(`npm view ${packageName} versions --json`).toString(),
 	).reverse();
 }
-export function listTags(registry, packageName, order, limitTo) {
+function listGithubReleases() {
+	return github
+		.getOctokit(
+			getInput('github_token', {
+				trimWhitespace: true,
+				required: true,
+			}),
+		)
+		.rest.repos.listReleases(github.context.repo);
+}
+export async function listTags(registry, packageName, order, limitTo) {
 	let tags = [];
 	switch (registry) {
 		case 'npm':
 			tags = listNPMTags(packageName);
 			if (order === 'asc') tags.reverse();
 			return tags.slice(0, limitTo);
+		case 'github':
+			return listGithubReleases();
 		default:
 			throw new Error(`registry ${registry} is not available`);
 	}
