c55060789caa83a531fb9ec3facbe48410619b1a
ShaMan123 <shacharnen@gmail.com>
2022-09-26 13:03:35 +0300

    test(): dotenv

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,16 @@
-import { listTags } from './util.js';
 import semver from 'semver';
+import { listTags } from './util.js';
+import * as dotenv from 'dotenv';
+// https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+dotenv.config();
 
-// console.log(listTags('npm', '  fabric ', 'desc', 10).length > 0);
-console.log(semver.satisfies('1.2.3', ''));
-console.log(semver.satisfies('0.1.19', '>=0.1.20'));
+async function test() {
+	console.log(await listTags('npm', '  fabric ', 'desc', 10));
+	console.log(
+		await listTags('github', 'ShaMan123/react-native-math-view', 'desc', 10),
+	);
+	console.log(semver.satisfies('1.2.3', ''));
+	console.log(semver.satisfies('0.1.19', '>=0.1.20'));
+}
+
+test();
