592f12d821e370d6db80219359cb5ca3633b83ca
Shachar <34343793+ShaMan123@users.noreply.github.com>
2022-09-26 10:59:43 +0300

    feat(): semver filtering + fix `order` (#3)
    * feat(): semver
    
    * fix(): order
    
    * Update index.js
    
    * Update main.cjs
    
    * Update index.js
    
    * Update main.cjs
    
    * Update publish.yml
    
    * rename
    
    * Update index.test.js
    
    * Update update_bug_report.yml

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,6 @@
 import { listTags } from './util.js';
+import semver from 'semver';
 
-console.log(listTags('npm', '  fabric ', 'desc', 10));
+// console.log(listTags('npm', '  fabric ', 'desc', 10).length > 0);
+console.log(semver.satisfies('1.2.3', ''));
+console.log(semver.satisfies('0.1.19', '>=0.1.20'));
