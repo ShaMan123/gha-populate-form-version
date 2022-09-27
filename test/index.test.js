6dc6be8bf936468cccf1b6bf5516a4cd4ba3afa2
ShaMan123 <shacharnen@gmail.com>
2022-09-27 08:32:26 +0300

    test(): workflow_dispatch

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
-import assert from 'assert';
+import github from '@actions/github';
+import assert from 'node:assert/strict';
+import { execSync } from 'child_process';
 import * as dotenv from 'dotenv';
+import sinon from 'sinon';
 import { listTags } from '../src/util.js';
+
+const branch = execSync('git branch --show-current').toString().trim();
 
 describe('action', function () {
 	before(() => {
 		// https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 		dotenv.config();
+	});
+	after(() => {
+		sinon.restore();
 	});
 	it('npm by package', async function () {
 		assert.ok(
@@ -17,6 +25,18 @@ describe('action', function () {
 		assert.ok(
 			(await listTags('github', 'ShaMan123/react-native-math-view')).length > 0,
 			'should fetch tags from github by user and repo',
+		);
+	});
+	it('workflow_dispatch', async function () {
+		await assert.doesNotReject(
+			github
+				.getOctokit(process.env.GITHUB_TOKEN)
+				.rest.actions.createWorkflowDispatch({
+					owner: 'ShaMan123',
+					repo: 'gha-populate-form-version',
+					ref: branch,
+					workflow_id: '.github/workflows/update_bug_report.yml',
+				}),
 		);
 	});
 });
